45cf60a5918efe85e3a4dbe7fa94d8383360ce4b
Lauraczuluagac <lauris.0208@gmail.com>
2023-11-04 17:09:04 -0500

    bfbr

--- pages/2_Aplicación 1.py
+++ pages/2_Aplicación 1.py
@@ -1,4 +1,23 @@
 import streamlit as st
+import pandas as pd
+
 
 # Diseño personalizado
-st.header("Aplicacón 1")
+st.header("Filtro 1 - resultado por comunas")
+
+df = pd.read_csv('historico.csv')
+
+comunas = df.loc[:,"comuna"].unique()
+
+comuna = st.selectbox('comunas',comunas)
+
+informacionComuna = df[df["comuna"] == comuna]
+
+puntajesGlobals = informacionComuna.loc[:,["codigo_dane","puntaje_global"]]
+
+st.bar_chart(puntajesGlobals.set_index('codigo_dane'))
+
+
+st.header("Filtro 2 - ")
+
+df = pd.read_csv('historico.csv')
